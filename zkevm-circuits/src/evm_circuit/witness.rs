61dd86e3535a9a6a75cc697dd3c637f0df03f1b4
SwordArt0820 <sendthefuture@outlook.com>
2022-02-27 00:53:50 +0800

    merge div/mod and mul into one gadget.

--- zkevm-circuits/src/evm_circuit/witness.rs
+++ zkevm-circuits/src/evm_circuit/witness.rs
@@ -1084,7 +1084,7 @@ impl From<&bus_mapping::circuit_input_builder::ExecStep> for ExecutionState {
         }
         match step.op {
             OpcodeId::ADD => ExecutionState::ADD,
-            OpcodeId::MUL => ExecutionState::MUL,
+            OpcodeId::MUL | OpcodeId::DIV | OpcodeId::MOD => ExecutionState::MUL,
             OpcodeId::SUB => ExecutionState::ADD,
             OpcodeId::EQ | OpcodeId::LT | OpcodeId::GT => ExecutionState::CMP,
             OpcodeId::SLT | OpcodeId::SGT => ExecutionState::SCMP,
