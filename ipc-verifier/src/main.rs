7f4cdaa436a8358a2e7a834d5b80de51f09e5133
Jules de Smit <julesdesmit@gmail.com>
2022-03-15 00:56:24 -0400

    Add wrapper for EvmCircuit so we can instrument it

--- ipc-verifier/src/main.rs
+++ ipc-verifier/src/main.rs
@@ -8,7 +8,14 @@ use std::fs;
 use std::io::{Cursor, Error as IOError, Read, Write};
 use std::os::unix::net::UnixListener;
 use std::path::Path;
-use zkevm_circuits::{evm_circuit::EvmCircuit, state_circuit::StateCircuit};
+use zkevm_circuits::state_circuit::StateCircuit;
+
+use eth_types::Field;
+use halo2_proofs::{
+    circuit::{Layouter, SimpleFloorPlanner},
+    plonk::{Circuit, ConstraintSystem, Error, Expression},
+};
+use zkevm_circuits::evm_circuit::{witness::Block, EvmCircuit};
 
 const SOCKET_PATH: &'static str = "/tmp/verifier.sock";
 // TODO: what should this be?
@@ -32,7 +39,7 @@ fn main() {
     // Set up params
     // TODO: this should probably come from a transcript file later on
     let general_params: Params<G1Affine> = Params::<G1Affine>::unsafe_setup::<Bn256>(DEGREE);
-    let evm_circuit = EvmCircuit::<Fr>::configure();
+    let evm_circuit = TestCircuit::<Fr>::default();
     // TODO: where do i get the config?
     let state_circuit = StateCircuit::<Fr, true, 49152, 16384, 2000, 16384, 1300, 16384>::default();
 
@@ -110,4 +117,50 @@ fn verify_proofs(
         &mut verifier_transcript,
     )
     .is_ok()
+}
+
+/////////////////////////////////////////////////////////////////////////
+// I'm implementing this here so that I can actually use EvmCircuit    //
+// This should be removed once Circuit is actually implemented for it. //
+/////////////////////////////////////////////////////////////////////////
+
+#[derive(Debug, Default)]
+pub struct TestCircuit<F> {
+    block: Block<F>,
+}
+
+impl<F: Field> Circuit<F> for TestCircuit<F> {
+    type Config = EvmCircuit<F>;
+    type FloorPlanner = SimpleFloorPlanner;
+
+    fn without_witnesses(&self) -> Self {
+        Self::default()
+    }
+
+    fn configure(meta: &mut ConstraintSystem<F>) -> Self::Config {
+        let tx_table = [(); 4].map(|_| meta.advice_column());
+        let rw_table = [(); 10].map(|_| meta.advice_column());
+        let bytecode_table = [(); 4].map(|_| meta.advice_column());
+        let block_table = [(); 3].map(|_| meta.advice_column());
+        // Use constant expression to mock constant instance column for a more
+        // reasonable benchmark.
+        let power_of_randomness = [(); 31].map(|_| Expression::Constant(F::one()));
+
+        EvmCircuit::configure(
+            meta,
+            power_of_randomness,
+            tx_table,
+            rw_table,
+            bytecode_table,
+            block_table,
+        )
+    }
+
+    fn synthesize(
+        &self,
+        config: Self::Config,
+        mut layouter: impl Layouter<F>,
+    ) -> Result<(), Error> {
+        config.assign_block(&mut layouter, &self.block)
+    }
 }
