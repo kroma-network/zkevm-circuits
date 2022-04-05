88258595829b5c6cbdffbebaf714c5bb51da9d8c
Zhang Zhuo <mycinbrin@gmail.com>
2022-04-05 19:03:02 +0800

    fix conflict

--- zkevm-circuits/src/evm_circuit/execution/shr.rs
+++ zkevm-circuits/src/evm_circuit/execution/shr.rs
@@ -74,6 +74,7 @@ mod test {
     use crate::test_util::run_test_circuits;
     use eth_types::evm_types::OpcodeId;
     use eth_types::{bytecode, Word};
+    use mock::TestContext;
     use rand::Rng;
 
     fn test_ok(opcode: OpcodeId, a: Word, shift: Word) {
@@ -84,7 +85,13 @@ mod test {
             .write_op(opcode)
             STOP
         };
-        assert_eq!(run_test_circuits(bytecode), Ok(()));
+        assert_eq!(
+            run_test_circuits(
+                TestContext::<2, 1>::simple_ctx_with_bytecode(bytecode).unwrap(),
+                None
+            ),
+            Ok(())
+        );
     }
 
     #[test]
