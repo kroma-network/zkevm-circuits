546385cbb447a44f37630f557e975d0dff8f2c56
Zhang Zhuo <mycinbrin@gmail.com>
2022-08-15 20:13:55 +0800

    Revert "tmp mute prev consistency"
    This reverts commit eb77bde070bbb49eee50492d810681183198599e.

--- zkevm-circuits/src/state_circuit/constraint_builder.rs
+++ zkevm-circuits/src/state_circuit/constraint_builder.rs
@@ -124,19 +124,19 @@ impl<F: Field> ConstraintBuilder<F> {
 
         // When at least one of the keys (tag, id, address, field_tag, or storage_key)
         // in the current row differs from the previous row.
-        //        self.condition(q.first_access(), |cb| {
-        //          cb.require_zero(
-        //            "first access reads don't change value",
-        //          q.is_read() * (q.rw_table.value.clone() - q.initial_value()),
-        //    );
-        //});
+        self.condition(q.first_access(), |cb| {
+            cb.require_zero(
+                "first access reads don't change value",
+                q.is_read() * (q.rw_table.value.clone() - q.initial_value()),
+            );
+        });
 
         // When all the keys in the current row and previous row are equal.
         self.condition(q.not_first_access.clone(), |cb| {
-            //            cb.require_zero(
-            //              "non-first access reads don't change value",
-            //            q.is_read() * (q.rw_table.value.clone() -
-            // q.rw_table.value_prev.clone()),      );
+            cb.require_zero(
+                "non-first access reads don't change value",
+                q.is_read() * (q.rw_table.value.clone() - q.rw_table.value_prev.clone()),
+            );
             cb.require_zero(
                 "initial value doesn't change in an access group",
                 q.initial_value.clone() - q.initial_value_prev(),
