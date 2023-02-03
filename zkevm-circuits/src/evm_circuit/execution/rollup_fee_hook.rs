5187f74967a1675c89c6855551df4eb99b4f10e2
Wonyong Kim(Ryan Kim) <ryan@lightscale.io>
2023-02-03 12:38:55 +0900

    fix(zkevm-circuits): assign missing witnesses to RollupFeeHook gadget

--- zkevm-circuits/src/evm_circuit/execution/rollup_fee_hook.rs
+++ zkevm-circuits/src/evm_circuit/execution/rollup_fee_hook.rs
@@ -6,7 +6,7 @@ use crate::{
             common_gadget::UpdateBalanceGadget,
             constraint_builder::{ConstraintBuilder, StepStateTransition, Transition::Delta},
             math_gadget::{AddWordsGadget, MulAddWordsGadget},
-            CachedRegion, Cell,
+            CachedRegion, Cell, Word,
         },
         witness::{Block, Call, ExecStep, Transaction},
     },
@@ -15,13 +15,22 @@ use crate::{
 };
 use eth_types::{
     kanvas_params::{L1_COST_DENOMINATOR, L1_FEE_RECIPIENT},
-    Field, ToScalar,
+    Field, ToLittleEndian, ToScalar,
 };
 use halo2_proofs::{circuit::Value, plonk::Error};
 
 #[derive(Clone, Debug)]
 pub(crate) struct RollupFeeHookGadget<F> {
     tx_id: Cell<F>,
+    l1_base_fee: Word<F>,
+    l1_fee_overhead: Word<F>,
+    l1_fee_scalar: Word<F>,
+    l1_gas_to_use: Word<F>,
+    zero: Word<F>,
+    l1_fee_tmp: Word<F>,
+    l1_fee_tmp2: Word<F>,
+    l1_cost_denominator: Word<F>,
+    l1_cost_remainder: Word<F>,
     add_rollup_data_gas_by_l1_fee_overhead: AddWordsGadget<F, 2, true>,
     mul_l1_gas_to_use_by_l1_base_fee: MulAddWordsGadget<F>,
     mul_l1_fee_tmp_by_l1_fee_scalar: MulAddWordsGadget<F>,
@@ -44,7 +53,7 @@ impl<F: Field> ExecutionGadget<F> for RollupFeeHookGadget<F> {
         let l1_gas_to_use = cb.query_word();
         let add_rollup_data_gas_by_l1_fee_overhead = AddWordsGadget::construct(
             cb,
-            [tx_rollup_data_gas_cost, l1_fee_overhead],
+            [tx_rollup_data_gas_cost, l1_fee_overhead.clone()],
             l1_gas_to_use.clone(),
         );
 
@@ -96,6 +105,15 @@ impl<F: Field> ExecutionGadget<F> for RollupFeeHookGadget<F> {
 
         Self {
             tx_id,
+            l1_base_fee,
+            l1_fee_overhead,
+            l1_fee_scalar,
+            l1_gas_to_use,
+            zero,
+            l1_fee_tmp,
+            l1_fee_tmp2,
+            l1_cost_denominator,
+            l1_cost_remainder,
             add_rollup_data_gas_by_l1_fee_overhead,
             mul_l1_gas_to_use_by_l1_base_fee,
             mul_l1_fee_tmp_by_l1_fee_scalar,
@@ -119,9 +137,17 @@ impl<F: Field> ExecutionGadget<F> for RollupFeeHookGadget<F> {
 
         self.tx_id
             .assign(region, offset, Value::known(F::from(tx.id as u64)))?;
+        self.l1_base_fee
+            .assign(region, offset, Some(block.l1_base_fee.to_le_bytes()))?;
+        self.l1_fee_overhead
+            .assign(region, offset, Some(block.l1_fee_overhead.to_le_bytes()))?;
+        self.l1_fee_scalar
+            .assign(region, offset, Some(block.l1_fee_scalar.to_le_bytes()))?;
 
         let rollup_data_gas_cost = eth_types::Word::from(tx.rollup_data_gas_cost);
         let l1_gas_to_use = rollup_data_gas_cost + block.l1_fee_overhead;
+        self.l1_gas_to_use
+            .assign(region, offset, Some(l1_gas_to_use.to_le_bytes()))?;
         self.add_rollup_data_gas_by_l1_fee_overhead.assign(
             region,
             offset,
@@ -129,29 +155,29 @@ impl<F: Field> ExecutionGadget<F> for RollupFeeHookGadget<F> {
             l1_gas_to_use,
         )?;
         let l1_fee_tmp = l1_gas_to_use * block.l1_base_fee;
+        let zero = eth_types::Word::zero();
+        self.zero.assign(region, offset, Some(zero.to_le_bytes()))?;
+        self.l1_fee_tmp
+            .assign(region, offset, Some(l1_fee_tmp.to_le_bytes()))?;
         self.mul_l1_gas_to_use_by_l1_base_fee.assign(
             region,
             offset,
-            [
-                l1_gas_to_use,
-                block.l1_base_fee,
-                eth_types::Word::zero(),
-                l1_fee_tmp,
-            ],
+            [l1_gas_to_use, block.l1_base_fee, zero, l1_fee_tmp],
         )?;
         let l1_fee_tmp2 = l1_fee_tmp * block.l1_fee_scalar;
+        self.l1_fee_tmp2
+            .assign(region, offset, Some(l1_fee_tmp2.to_le_bytes()))?;
         self.mul_l1_fee_tmp_by_l1_fee_scalar.assign(
             region,
             offset,
-            [
-                l1_fee_tmp,
-                block.l1_fee_scalar,
-                eth_types::Word::zero(),
-                l1_fee_tmp2,
-            ],
+            [l1_fee_tmp, block.l1_fee_scalar, zero, l1_fee_tmp2],
         )?;
         let l1_cost_denominator = *L1_COST_DENOMINATOR;
         let (l1_fee, l1_cost_remainder) = l1_fee_tmp2.div_mod(l1_cost_denominator);
+        self.l1_cost_denominator
+            .assign(region, offset, Some(l1_cost_denominator.to_le_bytes()))?;
+        self.l1_cost_remainder
+            .assign(region, offset, Some(l1_cost_remainder.to_le_bytes()))?;
         self.div_l1_fee_by_l1_cost_denominator.assign(
             region,
             offset,
